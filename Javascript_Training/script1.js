e468e843d7602bbe986b87bd8e8af2c77a9f5965
Siddharth <siddharth5135@gmail.com>
2026-03-05 16:34:11 +0530

    caculator

---
+++ Javascript_Training/script1.js
@@ -0,0 +1,10 @@
+function submitForm() {
+
+    const n = form.name.value;
+
+    console.log("Form Submitted Successfully!");
+    console.log("Name:", n);
+    console.log("Email:", email);
+    console.log("Gender:", gender);
+
+}
